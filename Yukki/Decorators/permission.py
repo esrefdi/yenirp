4fc2e706ee16a015c4455383287788f27308fe9a
Esreffmeherremov <101807789+esrefdi@users.noreply.github.com>
2022-03-21 15:54:16 +0400

    update permission.py

--- Yukki/Decorators/permission.py
+++ Yukki/Decorators/permission.py
@@ -10,27 +10,27 @@ def PermissionCheck(mystic):
         a = await app.get_chat_member(message.chat.id, BOT_ID)
         if a.status != "administrator":
             return await message.reply_text(
-                "I need to be admin with some permissions:\n"
-                + "\n- **can_manage_voice_chats:** To manage voice chats"
-                + "\n- **can_delete_messages:** To delete Bot's Searched Waste"
-                + "\n- **can_invite_users**: For inviting assistant to chat."
+                "Bəzi icazələrlə admin olmalıyam:\n"
+                + "\n- **can_manage_voice_chats:** Səsli söhbətləri idarə etmək üçün ♻️"
+                + "\n- **can_delete_messages:** Botun Axtardığı Tullantıları silmək üçün 🚮"
+                + "\n- **can_invite_users**: Köməkçini söhbətə dəvət etmək üçün."
             )
         if not a.can_manage_voice_chats:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __MANAGE VOICE CHATS__"
+                "Bu əməliyyatı yerinə yetirmək üçün tələb olunan icazəm yoxdur."
+                + "\n**İcazə:** __MANAGE VOICE CHATS__"
             )
             return
         if not a.can_delete_messages:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __DELETE MESSAGES__"
+                "Bu əməliyyatı yerinə yetirmək üçün tələb olunan icazəm yoxdur."
+                + "\n**İcazə:** __DELETE MESSAGES__"
             )
             return
         if not a.can_invite_users:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __INVITE USERS VIA LINK__"
+                "Bu əməliyyatı yerinə yetirmək üçün tələb olunan icazəm yoxdur."
+                + "\n**İcazə:** __INVITE USERS VIA LINK__"
             )
             return
         return await mystic(_, message)
